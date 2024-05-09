dd52f23edcb2c941df2ff53b1fd3175dc0587850
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-05-09 11:15:39 +0200

    Delete commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,70 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import GetImg from '../getImage';
+import GetImg from './getImage';
 
 function App() {
-  const [photos, setPhotos] = useState([])
-  const [photoId, setPhotoId] = useState([])
+  const [photos, setPhotos] = useState([]);
+  const [photoId, setPhotoId] = useState([]);
 
-  const [result, setResult] = useState(0)
-  const [bestResult, setBestResult] = useState(0)
+  const [result, setResult] = useState(0);
+  const [bestResult, setBestResult] = useState(0);
 
   let ignore = false;
 
-  useEffect(()=>{
-    GetImg().then(result =>{
-      if (!ignore) setPhotos(result)
-    })
+  useEffect(() => {
+    GetImg().then((result) => {
+      if (!ignore) setPhotos(result);
+    });
     return () => {
-      ignore = true;}
-  },[])
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ignore = true;
+    };
+  }, []);
 
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-    return array
-}
+    return array;
+  }
   function handleClick(e) {
-    const currentValue = e.currentTarget.dataset.value
-    
-    
-    if(photoId.includes(currentValue)){
-      if(result > bestResult)setBestResult(result)
-      setResult(0)
-      setPhotoId([])
-    }else {
-      setResult(result + 1)
-      setPhotoId(((photoId) => [...photoId, currentValue  ]) )
+    const currentValue = e.currentTarget.dataset.value;
+
+    if (photoId.includes(currentValue)) {
+      if (result > bestResult) setBestResult(result);
+      setResult(0);
+      setPhotoId([]);
+    } else {
+      setResult(result + 1);
+      setPhotoId((photoId) => [...photoId, currentValue]);
     }
 
-    setPhotos(shuffleArray(photos))
-   
-    
-    if (currentValue === "25") document.querySelector(".my-audio").play()
-    
-    
-    
-    
+    setPhotos(shuffleArray(photos));
+
+    if (currentValue === '25') document.querySelector('.my-audio').play();
   }
   return (
     <>
-    <div className="header">
-      <div className="title">
-        <h1>Pokemon Memory Card Game</h1>
-        <p>Click only one time on the same picture and get point</p>
-
-      </div>
-      <div className="score">
-      <p>Score: {result}</p>
-      <p>Best Score: {bestResult}</p>
-      </div>
-    </div>
-    <div className='cards'>
-      {photos.map((photo) => (
-        <div key={photo.id} onClick={handleClick} data-value={photo.id}>
-          <img src={photo.url} alt={photo.title} width={300} />
-          <p>{photo.title}</p>
+      <div className="header">
+        <div className="title">
+          <h1>Pokemon Memory Card Game</h1>
+          <p>Click only one time on the same picture and get point</p>
         </div>
-      ))}
-    </div>
-    <audio className='my-audio' src="/pikachu.ogg" type="audio/ogg"></audio>
+        <div className="score">
+          <p>Score: {result}</p>
+          <p>Best Score: {bestResult}</p>
+        </div>
+      </div>
+      <div className="cards">
+        {photos.map((photo) => (
+          <div key={photo.id} onClick={handleClick} data-value={photo.id}>
+            <img src={photo.url} alt={photo.title} width={300} />
+            <p>{photo.title}</p>
+          </div>
+        ))}
+      </div>
+      <audio className="my-audio" src="/pikachu.ogg" type="audio/ogg"></audio>
     </>
   );
 }
---
+++ src/getImage.jsx
@@ -0,0 +1,20 @@
+export default async function GetImg() {
+  const photos = [];
+  const imgNumber = 12;
+  const pikachuNumber = 25;
+
+  for (let i = pikachuNumber; i < pikachuNumber + imgNumber; i += 1) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`, {
+      mode: 'cors',
+    });
+    const imgData = await response.json();
+    const photo = {
+      id: i,
+      url: imgData.sprites.other['official-artwork'].front_default,
+      title: imgData.name,
+    };
+    photos.push(photo);
+  }
+
+  return photos;
+}
