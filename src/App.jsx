bd86f8c3f8f8cdb8c93c2a9af22fe3d4978e6940
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-05-09 09:25:13 +0200

    Add states & function to shuffle array

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import GetImg from '../getImage';
 
 function App() {
   const [photos, setPhotos] = useState([])
+  const [photoId, setPhotoId] = useState([])
 
   const [result, setResult] = useState(0)
   const [bestResult, setBestResult] = useState(0)
@@ -18,7 +19,31 @@ function App() {
       ignore = true;}
   },[])
 
-  console.log(photos)
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+  function handleClick(e) {
+    const currentValue = e.currentTarget.dataset.value
+
+    
+    if(photoId.includes(currentValue)){
+      if(result > bestResult) setBestResult(result)
+      setResult(0)
+      setPhotoId([])
+    }else {
+      setResult(result + 1)
+      setPhotoId(((photoId) => [...photoId, currentValue  ]) )
+    }
+
+    setPhotos(shuffleArray(photos))
+
+    
+    
+  }
   return (
     <>
     <div className="results">
@@ -27,7 +52,7 @@ function App() {
     </div>
     <div className='cards'>
       {photos.map((photo) => (
-        <div key={photo.id}>
+        <div key={photo.id} onClick={handleClick} data-value={photo.id}>
           <img src={photo.url} alt={photo.title} width={300} />
           <p>{photo.title}</p>
         </div>
